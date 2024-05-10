2337754bdf299902936c9c3e3358838b9ad2784e
Minji Kim <minjikim2000@hotmail.com>
2024-05-11 04:58:13 +0900

    feat: 모바일 화면 크기를 가져오는 기능 추가 #2

--- aboutMe/src/App.jsx
+++ aboutMe/src/App.jsx
@@ -1,6 +1,15 @@
 import Router from "./Router";
+import { useEffect } from "react";
 
 function App() {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
+
   return (
     <>
       <Router />
