defc804a08055d884542db28e2e467559069782d
yyyujinnn <1209yujin@naver.com>
2024-05-13 00:14:37 +0900

    fix: SigninPage 반응형에 맞게 css 변경

--- aboutMe/src/pages/SigninPage.jsx
+++ aboutMe/src/pages/SigninPage.jsx
@@ -2,17 +2,23 @@ import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import AboutMeLogo from '../assets/AboutMeLogo.png';
 
-const MainBody = styled.div`
-  width: 390px;
-  height: 844px;
+const MainBody = styled.div`  
+  // width: 390px;
+  // height: 844px;
 `;
 
-const LogoImage = styled.img`
-  margin-top: 150px;
+const LogoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+  height: 58vh;
 `;
 
-const InputWrapper = styled.div`
-  margin-top: 200px;
+const InputContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
 const Input = styled.input`
@@ -23,7 +29,17 @@ const Input = styled.input`
   font-size: 15px;
 `;
 
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+`;
+
 const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   width: 370px;
   height: 65px;
   margin-top: 40px;
@@ -35,6 +51,9 @@ const Button = styled.button`
 `;
 
 const SignupText = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center; 
   font-size: 15px;
   color: #A5A5A5;
   text-decoration: underline;
@@ -50,13 +69,21 @@ const SigninPage = () => {
 
   return (
     <MainBody>
-      <LogoImage src={AboutMeLogo} alt='logo'/>
-      <InputWrapper>
+
+      <LogoContainer className='logo'>
+        <img src={AboutMeLogo} alt='logo'/>
+      </LogoContainer>
+
+      <InputContainer className='loginBox'>
         <Input placeholder='아이디' />
         <Input placeholder='비밀번호' type='password' />
-      </InputWrapper>
-      <Button> 입장하기 </Button>
+      </InputContainer>
+
+      <ButtonContainer>
+        <Button className='btnlogin'> 입장하기 </Button>
+      </ButtonContainer>
       <SignupText onClick={handleSignupClick}>회원가입 하기</SignupText>
+      
     </MainBody>
   )
 }
