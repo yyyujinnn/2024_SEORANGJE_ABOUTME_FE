90faa5606c12ac10ae06dc6e6a9fe1c0b3af0157
Sohyunnnn <thgus05061@naver.com>
2024-05-14 00:05:11 +0900

    feat: 더미데이터 연결 #5

---
+++ aboutMe/src/api/diary/diaryDummy.js
@@ -0,0 +1,7 @@
+export const dummyData = [
+  { imageUrl: 'DummyImage1', title: '너랑 어울리는 과일' },
+  { imageUrl: 'DummyImage2', title: '너랑 어울리는 꽃' },
+  { imageUrl: 'DummyImage3', title: '너랑 어울리는 장소' },
+  { imageUrl: 'DummyImage4', title: '너랑 어울리는 동물' },
+  { imageUrl: 'DummyImage5', title: '너랑 어울리는 계절' },
+];
