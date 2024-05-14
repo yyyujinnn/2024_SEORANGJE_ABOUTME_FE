6117ea3b151300e63c709ec845653d30228c0be5
Minji Kim <minjikim2000@hotmail.com>
2024-05-15 04:54:11 +0900

    feat: 업로드 기능 빼고 UI 구현

--- aboutMe/src/pages/MakingPage.jsx
+++ aboutMe/src/pages/MakingPage.jsx
@@ -8,8 +8,6 @@ import img3 from "../assets/image3.jpg";
 import img4 from "../assets/image4.jpg";
 import img5 from "../assets/image5.jpg";
 
-const images = [img1, img2, img3, img4, img5];
-
 const ScreenContainer = styled.div`
   overflow-x: hidden;
   display: flex;
@@ -17,7 +15,7 @@ const ScreenContainer = styled.div`
   align-items: center;
   width: 100%;
   height: calc(var(--vh, 1vh) * 100);
-  background: #ff8caf;
+  //background: #ff8caf;
 `;
 const RowWrapper = styled.div`
   display: flex;
@@ -35,11 +33,13 @@ const SmallTextBox = styled.div`
   border-radius: 28px;
   background: linear-gradient(180deg, #d3fcff 0%, #fff 133.93%);
   text-align: center;
+  font-family: "NeoDunggeunmo";
   font-size: 20px;
   font-weight: 400;
   color: #000;
 `;
 const TitleText = styled.div`
+  font-family: "NeoDunggeunmo";
   font-size: 40px;
   font-weight: 400;
   color: #000;
@@ -50,19 +50,25 @@ const WhiteWrapper = styled.div`
   white-space: pre;
 `;
 const QuestionContainer = styled.div`
-  padding: 10px 16px 18px 16px;
+  width: 332px;
+  padding: 10px 14px 18px 14px;
   border: 1px solid #000;
+  box-sizing: border-box;
   background: linear-gradient(180deg, #c0ee41 0%, #fff 133.93%);
   color: #000;
+  font-family: "NeoDunggeunmo";
   font-size: 20px;
-  font-weight: 400;
-  white-space: pre;
+  line-height: 30px;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  word-break: break-all;
 `;
 const PictureContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  width: 332px;
   margin: 10px 0;
   padding: 22px 0 40px 0;
   border: 1px solid #000;
@@ -72,9 +78,8 @@ const PictureText = styled.div`
   color: #ffc700;
   text-align: center;
   text-shadow: -0.3px 0px black, 0px 0.3px black, 0.3px 0px black, 0px -0.3px black;
-  font-family: "S-Core Dream";
+  font-family: "S-CoreDreams";
   font-size: 15px;
-  font-style: normal;
   font-weight: 300;
   line-height: normal;
 `;
@@ -99,17 +104,26 @@ const ArrowWrapper = styled(RowWrapper)`
   margin: 10px 0;
 `;
 const UploadText = styled.div`
-  margin: 0 10px;
+  width: 230px;
+  margin-right: 5px;
   padding: 20px;
   border: 1px solid #000;
   background: #fff;
+  font-family: "NeoDunggeunmo";
+  font-size: 13px;
+  text-align: center;
 `;
-const UploadButton = styled.button`
-  margin: 0;
-  padding: 8px 10px;
+const UploadInput = styled.input`
+  display: none;
+`;
+const UploadButton = styled.label`
+  width: 52px;
+  margin-left: 5px;
+  padding-top: 10px;
   border: 1px solid #000;
   background: #fff;
   font-size: 35px;
+  text-align: center;
   color: black;
 `;
 const ProgressContainer = styled(RowWrapper)`
@@ -118,6 +132,7 @@ const ProgressContainer = styled(RowWrapper)`
 const Text = styled.div`
   color: #000;
   text-align: center;
+  font-family: "NeoDunggeunmo";
   font-size: 13px;
   font-weight: 400;
 `;
@@ -129,10 +144,10 @@ const ProgressBarWrapper = styled.div`
   background: #fff;
 `;
 const ProgressBar = styled.div`
-  width: 34px;
+  width: 27px;
   height: 9px;
   background-color: ${(props) => (props.isActive ? "#C1EE44" : "#CCCCCC")};
-  margin: 2px;
+  margin: 1px;
 `;
 const CircleLeftArrow = styled(LeftArrow)`
   border-radius: 50%;
@@ -142,15 +157,139 @@ const CircleRightArrow = styled(RightArrow)`
   border-radius: 50%;
   background-color: white;
 `;
+const WritingContainer = styled.div`
+  position: relative;
+  width: 332px;
+  height: 347px;
+  margin: 10px 0;
+  background: linear-gradient(transparent 39px, #ccc 24px);
+  background-size: 100% 40px;
+  //padding: 20px;
+  border: 1px solid #000;
+`;
+const TextArea = styled.textarea`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+  background: transparent;
+  resize: none;
+  outline: none;
+  font-family: "NeoDunggeunmo";
+  font-size: 15px;
+  line-height: 40px; //WritingContainer하고 같아야 함
+  color: #333; /* Text color */
+  padding: 10px;
+  box-sizing: border-box;
+`;
+const SenderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 272px;
+  padding: 20px 30px;
+  border: 1px solid #000;
+  font-family: "NeoDunggeunmo";
+  font-size: 15px;
+  background: #fff;
+`;
+const SenderLabel = styled.span`
+  margin-right: 4px;
+`;
+const SenderInput = styled.input`
+  font-family: "NeoDunggeunmo";
+  font-size: 15px;
+  border: transparent;
+  outline: none;
+`;
+
+// 이미지 더미데이터 예시
+const categories = [
+  {
+    name: "음식을",
+    images: [img1, img2],
+    uploadedImages: [],
+  },
+  {
+    name: "장소를",
+    images: [img3, img4],
+    uploadedImages: [],
+  },
+  {
+    name: "동물을",
+    images: [img5, img1],
+    uploadedImages: [],
+  },
+  {
+    name: "장소를",
+    images: [img4, img4],
+    uploadedImages: [],
+  },
+  {
+    name: "캐릭터를",
+    images: [img5, img5],
+    uploadedImages: [],
+  },
+];
 
 const MakingPage = () => {
-  const [currentImg, setCurrentImg] = useState(0);
-  const [preview, setPreview] = useState();
-  const nextImage = () => {
-    setCurrentImg((prevIndex) => (prevIndex < images.length - 1 ? prevIndex + 1 : prevIndex));
+  const [currentCategory, setCurrentCategory] = useState(0);
+  const [currentImage, setCurrentImage] = useState(0);
+  const [image, setImage] = useState(categories[currentCategory].images);
+  const [showWriting, setWriting] = useState(false);
+  const [uploadedImages, setUploadedImages] = useState([]);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        const newImage = reader.result;
+        const updatedCategories = [...categories];
+        updatedCategories[currentCategory].uploadedImages.unshift(newImage);
+        setUploadedImages([newImage, ...uploadedImages]);
+        setImage([newImage, ...image]);
+        console.log(categories[currentCategory].images);
+        setCurrentImage(0);
+      }
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
   };
+
+  const prevCategory = () => {
+    if (showWriting) {
+      setCurrentCategory(currentCategory);
+      setWriting(false);
+    } else if (currentCategory > 0) {
+      setCurrentCategory(currentCategory - 1);
+      setCurrentImage(0); // 새로운 카테고리로 넘어갈 때 이미지 인덱스 초기화
+    }
+  };
+
+  const nextCategory = () => {
+    if (currentCategory < categories.length - 1) {
+      setCurrentCategory(currentCategory + 1);
+      setCurrentImage(0); // 새로운 카테고리로 넘어갈 때 이미지 인덱스 초기화
+    } else {
+      setWriting(true);
+    }
+  };
+
   const prevImage = () => {
-    setCurrentImg((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : prevIndex));
+    if (currentImage > 0) {
+      setCurrentImage(currentImage - 1);
+    }
+  };
+
+  const nextImage = () => {
+    if (currentImage < categories[currentCategory].images.length - 1) {
+      setCurrentImage(currentImage + 1);
+    }
   };
   return (
     <ScreenContainer>
@@ -161,32 +300,62 @@ const MakingPage = () => {
         </TitleText>
         <TitleText>보여줘!</TitleText>
       </TitleContainer>
-      <QuestionContainer>Q1{"\n"}주희와 어울리는 음식을 골라줘!</QuestionContainer>
-      <PictureContainer>
-        <PictureText>토마토</PictureText>
-        <ArrowWrapper>
-          <LeftArrow onClick={prevImage} />
-          <Image src={images[currentImg]} alt="img" />
-          <RightArrow onClick={nextImage} />
-        </ArrowWrapper>
-        <PictureText>동글동글 달짝지근, 여름을 닮은 토마토</PictureText>
-      </PictureContainer>
-      <RowWrapper>
-        <UploadText>원하는 이미지가 없다면 직접 올려봐!</UploadText>
-        <UploadButton type="file" onClick={handleImageChange} ref={fileInputRef}>
-          <IoCameraOutline />
-        </UploadButton>
-      </RowWrapper>
+      <QuestionContainer>
+        Q{!showWriting ? currentCategory + 1 : "6"}. {"\n"}
+        {!showWriting
+          ? `주희와 어울리는 ${categories[currentCategory].name} 골라줘!`
+          : "마지막으로 한마디를 남겨줘!"}
+      </QuestionContainer>
+      {!showWriting ? (
+        <>
+          <PictureContainer>
+            <PictureText>토마토</PictureText>
+            <ArrowWrapper>
+              <LeftArrow onClick={prevImage} />
+              <Image src={categories[currentCategory].images[currentImage]} alt="img" />
+              {/* {image.map((image, index) => (
+                <Image key={index} src={image} alt={`Image ${index}`} />
+              ))} */}
+              <RightArrow onClick={nextImage} />
+            </ArrowWrapper>
+            <PictureText>동글동글 달짝지근, 여름을 닮은 토마토</PictureText>
+          </PictureContainer>
+          <RowWrapper>
+            <UploadText>원하는 이미지가 없다면 직접 올려봐!</UploadText>
+            <UploadInput
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              id="upload-input"
+            />
+            <UploadButton htmlFor="upload-input">
+              <IoCameraOutline />
+            </UploadButton>
+          </RowWrapper>
+        </>
+      ) : (
+        <>
+          <WritingContainer>
+            <TextArea placeholder="여기에 하고싶은 말을 적어봐!"></TextArea>
+          </WritingContainer>
+          <SenderContainer>
+            <SenderLabel>From: </SenderLabel>
+            <SenderInput type="text" placeholder="발신자 이름" />
+          </SenderContainer>
+        </>
+      )}
       <ProgressContainer>
-        <CircleLeftArrow />
+        <CircleLeftArrow onClick={prevCategory} />
         <Text>이전</Text>
         <ProgressBarWrapper>
-          {images.map((_, index) => (
-            <ProgressBar key={index} isActive={index <= currentImg} />
+          {[...Array(5)].map((_, index) => (
+            <ProgressBar key={index} isActive={index <= currentCategory} />
           ))}
+          {/* 여섯 번째 칸은 텍스트로 처리 */}
+          <ProgressBar isActive={showWriting} />
         </ProgressBarWrapper>
         <Text>다음</Text>
-        <CircleRightArrow />
+        <CircleRightArrow onClick={nextCategory} />
       </ProgressContainer>
     </ScreenContainer>
   );
