c3c390487baa5d0d054f8682af7e8455f50c3a01
Minji Kim <minjikim2000@hotmail.com>
2024-05-20 16:55:34 +0900

    vite가 service worker 못부르는 오류

--- aboutMe/vite.config.js
+++ aboutMe/vite.config.js
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // 기본 출력 디렉토리
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        sw: "./sw.js",
+      },
+    },
   },
 });
