e7076a9eb7d3d6a7214eb87334f72a6e6eccf144
Sohyunnnn <thgus05061@naver.com>
2024-05-20 16:17:21 +0900

    feat: 배경 색 변경 및 타이틀 레이아웃 수정 #12

--- aboutMe/src/pages/DiaryPage.jsx
+++ aboutMe/src/pages/DiaryPage.jsx
@@ -15,22 +15,35 @@ const Title = styled.div`
 
 const GridContainer = styled.div`
   display: grid;
-  grid-template-columns: 0.1fr 1fr 0.1fr;
+  grid-template-columns: 20px 170px 20px;
   gap: 10px;
   align-items: center;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  background: linear-gradient(180deg, rgba(126, 246, 255, 0.40) 0.01%, rgba(255, 255, 255, 0.40) 21.42%, rgba(255, 255, 255, 0.40) 89.33%, rgba(126, 246, 255, 0.40) 99.99%);
+  position: absolute;
+  z-index: -1;
+  width: 100%
+`
+
+const FolderIcon= styled.img`
+  justify-self: end;
 `;
 
 const DiaryPage = () => {
 
   return (
     <main style={{ display: 'flex', flexDirection: 'column',alignItems:'center'}}>
-      
+      <Container>
       <GridContainer>
-        <img src={folderIcon} alt='folderIcon'/>
+        <FolderIcon src={folderIcon} alt='folderIcon'/>
         <Title>주희의 첫 번째 아이콘</Title>
         <img src={folderIcon} alt='folderIcon'/>
       </GridContainer>
       <Diary />
+      </Container>
     </main>
     
   )
