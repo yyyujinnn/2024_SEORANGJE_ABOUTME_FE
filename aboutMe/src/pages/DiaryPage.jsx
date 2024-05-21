87a96145136f7bc84fa9132d5679a1784cb46047
Sohyunnnn <thgus05061@naver.com>
2024-05-22 02:51:55 +0900

    feat: 뒤로가기 기능 추가 #23

--- aboutMe/src/pages/DiaryPage.jsx
+++ aboutMe/src/pages/DiaryPage.jsx
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 import folderIcon  from '../assets/DiaryPage/folderIcon.svg';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import UserInfo from '../api/UserInfo'
+import UserInfo from '../api/UserInfo';
+import back from '../assets/DiaryPage/back.png';
+import { useNavigate } from 'react-router-dom';
 
 
 const Title = styled.div`
@@ -19,23 +21,28 @@ const Title = styled.div`
 
 const GridContainer = styled.div`
   display: grid;
-  grid-template-columns: 20px 170px 20px;
+  grid-template-columns: 40px 20px 170px 20px 40px;
   gap: 10px;
   align-items: center;
   justify-content: center;
-  margin-bott0m: 10px;
 `;
 
 const Container = styled.div`
   background: linear-gradient(180deg, rgba(126, 246, 255, 0.40) 0.01%, rgba(255, 255, 255, 0.40) 21.42%, rgba(255, 255, 255, 0.40) 89.33%, rgba(126, 246, 255, 0.40) 99.99%);
   position: absolute;
   z-index: -1;
-  width: 100%
+  width: 100%;
+  height: 100%;
 `
 
 const FolderIcon= styled.img`
   justify-self: end;
 `;
+
+const Img = styled.img`
+  width: 7.5px;
+  padding-right: 50px;
+`
 
 const DiaryPage = () => {
 
@@ -45,6 +52,12 @@ const DiaryPage = () => {
   const { id } = useParams();
 
   const [name, setName] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate('/home');
+  };
 
   useEffect(() => {
     const fetchUserInfo = async () => {
@@ -61,16 +74,15 @@ const DiaryPage = () => {
   }, []);
 
   return (
-    <main style={{ display: 'flex', flexDirection: 'column',alignItems:'center'}}>
       <Container>
       <GridContainer>
+        <Img src={back} onClick={handleBackClick} alt="Back"/>
         <FolderIcon src={folderIcon} alt='folderIcon'/>
         <Title>{name}의 {iconIndex}번째 아이콘</Title>
         <img src={folderIcon} alt='folderIcon'/>
       </GridContainer>
       <Diary myimage_id={id}/>
       </Container>
-    </main>
     
   )
 }
