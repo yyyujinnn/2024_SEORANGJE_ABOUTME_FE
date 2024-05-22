86f5227b7071456e46ca8423ba966bfcdac51e44
Minji Kim <minjikim2000@hotmail.com>
2024-05-22 20:20:34 +0900

    fix: 게스트인 경우 이미지 생성 기능 추가

--- aboutMe/src/Router.jsx
+++ aboutMe/src/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import {
   DiaryPage,
@@ -7,62 +7,22 @@ import {
   ThemePage,
   MakingPage,
   HomePage,
-  GuestPage
-} from './pages';
-
+  GuestPage,
+} from "./pages";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path='/'
-          element={
-              <SigninPage />
-          }
-        />
-        <Route
-          path='/diary/:index/:id'
-          element={
-              <DiaryPage />
-          }
-        />
-        <Route
-          path='sign-in'
-          element={
-              <SigninPage />
-          }
-        />
-        <Route
-          path='/sign-up'
-          element={
-              <SignupPage />
-          }
-        />
-        <Route
-          path='/theme'
-          element={
-              <ThemePage />
-          }
-        />
-        <Route
-          path='/making'
-          element={
-              <MakingPage />
-          }
-        />
-        <Route
-          path='/home'
-          element={
-              <HomePage />
-          }
-        />
-        <Route
-          path='/:uuid'
-          element={
-              <GuestPage />
-          }
-        />
+        <Route path="/" element={<SigninPage />} />
+        <Route path="/diary/:index/:id" element={<DiaryPage />} />
+        <Route path="sign-in" element={<SigninPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
+        <Route path="/theme" element={<ThemePage />} />
+        <Route path="/making" element={<MakingPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/:uuid" element={<GuestPage />} />
+        <Route path="/:uuid/making" element={<MakingPage />} />
       </Routes>
     </BrowserRouter>
   );
