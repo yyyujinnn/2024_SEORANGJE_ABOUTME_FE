97af322d73214157b863418d19813ef7ca9dae67
yyyujinnn <1209yujin@naver.com>
2024-05-22 22:27:06 +0900

    fix: react-helmet 적용 오류 수정

--- aboutMe/src/App.jsx
+++ aboutMe/src/App.jsx
@@ -7,21 +7,28 @@ function App() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
   }
+  // useEffect(() => {
+  //   setScreenSize();
+  // });
   useEffect(() => {
     setScreenSize();
-  });
+    window.addEventListener('resize', setScreenSize);
+    return () => {
+      window.removeEventListener('resize', setScreenSize);
+    };
+  }, []);
 
   return (
     <>
     <Helmet>
       <meta property="og:title" content="AboutMe" />  
-      <meta property="og:image" content="../public/icons/AboutMeTmb.png" />
+      <meta property="og:image" content="/icons/AboutMeTmb.png" />
       {/* <meta property="og:url" content="https://aboutme.d1pt3wqvnh1bfv.amplifyapp.com/" /> */}
       <meta property="og:url" content="https://fix-inapp.dxpjcplfcmdfo.amplifyapp.com/" />
       <meta property="og:description" content="네フГ 생각ṑト는 ∟ト를 알㈃ㅕ줘! (੭•̀ᴗ•̀)੭" />
     
       <meta name="twitter:title" content="AboutMe" />
-      <meta name="twitter:image" content="../public/icons/AboutMeTmb.png" />    
+      <meta name="twitter:image" content="/icons/AboutMeTmb.png" />    
       <meta name="twitter:description" content="네フГ 생각ṑト는 ∟ト를 알㈃ㅕ줘! (੭•̀ᴗ•̀)੭" />
       </Helmet>
       <Router />
